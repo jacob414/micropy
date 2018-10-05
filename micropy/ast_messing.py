626965e55f6f31b2ec516cdd14f60b87302a880e
Jacob Oscarson <jacob@plexical.com>
2018-10-05 18:45:09 +0200

    Whoah!! Injicerat en lokal.

--- micropy/ast_messing.py
+++ micropy/ast_messing.py
@@ -5,27 +5,33 @@ from copy import copy
 
 import ast
 
-def messing(x):
+def messing():
     print('rad 1')
+    print(foo)
     return 'returvärde'
 
 def alien():
     print('alien 1!')
+    print('alien 2!')
+    foo = 'bar'
 
 if __name__ == '__main__':
     tree = patterns.get_ast(messing)
     body = tree.body[0].body
-    call = ast.Expr(value=helpers.make_call('alien'))
+    # call = ast.Expr(value=helpers.make_call('alien'))
 
     abody = patterns.get_ast(alien).body
-    body.insert(0, abody[0].body[0])
+    idx = 0
+    for stmnt in abody[0].body:
+        body.insert(idx, stmnt)
+        idx += 1
 
-    # body.insert(0, call)
+    # body.insert(0, abody[0].body[0])
 
     local = {}
     ast.fix_missing_locations(tree)
     code = compile(tree, 'foo', 'single')
-    eval(code, {'alien': alien}, local)
+    eval(code, {}, local)
 
-    print(local['messing'](1))
+    local['messing']()
 
