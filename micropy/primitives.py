cf7b71144c9067b065b54543ca1dc7731fe40dfa
Jacob Oscarson <jacob@414soft.com>
2019-03-01 10:26:08 +0100

    Imported the memoize() technique used Juntoo

--- micropy/primitives.py
+++ micropy/primitives.py
@@ -72,3 +72,15 @@ def raises(ExcType):
         raise ExcType(*args, **kwargs)
 
     return raiser
+
+# http://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+class memoize(dict):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args):
+        return self[args]
+
+    def __missing__(self, key):
+        result = self[key] = self.func(*key)
+        return result
