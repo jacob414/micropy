0167c4a2c31c941d022d0ff849162360b7020187
Jacob Oscarson <jacob@414soft.com>
2019-09-25 15:25:12 +0200

    Fixes small errors in package installation

--- setup.py
+++ setup.py
@@ -12,30 +12,27 @@ import micropy
 install = (
     "funcy>=1.10.2",
     "pysistence>=0.4.1",
-    "patterns>=0.3"
+    "patterns>=0.3",
 )  # yapf: disable
 
 develop = (
     "pytest>=5.0.1",
+    "hypothesis>=4.24.3",
+    "altered_states>=1.0.9",
 )  # yapf: disable
-
-if sys.version_info.major < 3:
-    from micropy import depend_2019
-    install = install + depend_2019.backports
 
 setup(
     name='micropy',
     version=micropy.__version__,
     description="Some Python nicieties",
-    package=('micropy', ),
+    packages=('micropy', ),
     author='Jacob Oscarson',
     author_email='jacob@414soft.com',
     install_requires=install,
     extras_require={
         'test': install + develop,
     },
-    test_require=install + develop,
-    licence='MIT',
+    license='MIT',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
